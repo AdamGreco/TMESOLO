62b211bd7138bdc6b8e51940e27b7445d8ea50d1
AdamGreco <47328617+AdamGreco@users.noreply.github.com>
2019-04-15 17:48:35 +0200

    Add files via upload

---
+++ strategie.py
@@ -0,0 +1,83 @@
+""" les strategies prennent tous un superviseur. On joue avec les encoders et selon la strategie,
+on essaie d'en tirer une certaine vitesse et vitesse angulaire qu'on transmettra au superviseur
+"""
+
+class LigneStrat():
+    """cette strategie consiste a faire avancer le robot en ligne droite
+    sur une certaine distance donnee en parametre
+    """
+
+    def __init__(self,superviseur,distancemax):
+
+        self.superviseur=superviseur
+        self.distance=0
+        self.distancemax=distancemax
+        self.superviseur.robot.reset_encoder()
+
+    def get_command(self):
+
+        #si on a pas atteint la distance fixee
+        if (self.distance < self.distancemax):
+            self.distance+= self.superviseur.robot.get_encoder()[0]*self.superviseur.robot.rayonroue
+            return self.superviseur.robot.vmax, 0
+        else:
+            return 0,0
+         
+
+
+class Turn90Strat():
+    """on tourne de 90 degrees
+    """
+
+    def __init__(self,superviseur):
+
+        self.superviseur=superviseur
+        self.superviseur.robot.reset_encoder()
+        self.angle_parcouru=0
+
+    def get_command(self):
+
+        if (self.angle_parcouru < 82):  #on a une imprecision de 8 degrees dans la simulation et 
+                                        #il faut 82 degrees pour en faire 90, la source du probleme
+                                        #doit encore etre trouvee
+            self.angle_parcouru += self.superviseur.robot.get_encoder()[0]
+            print(self.angle_parcouru)
+            return 0,10
+        else:
+            return 0,0
+
+class SquareStrat():
+    """on fait dessiner un carre au robot
+    """
+
+    def __init__(self,superviseur):
+
+        self.superviseur= superviseur
+
+        #on a besoin de sous strategies ici
+        self.strat_avancer = LigneStrat(self.superviseur,5)
+        self.strat_tourner= Turn90Strat(self.superviseur)
+
+    def get_command(self):
+        #on fait alterner les sous strategies et on
+        #base notre raisonnement sur leurs reponses respectives,
+        #si une sous strategie renvoie 0,0 on passe a la sous strategie
+        #suivante
+
+        v_avancer, omega_avancer = self.strat_avancer.get_command()
+
+        if (v_avancer==0 and omega_avancer==0):
+            v_tourner, omega_tourner = self.strat_tourner.get_command()
+
+            if (v_tourner==0 and omega_tourner==0):
+                self.strat_avancer=LigneStrat(self.superviseur,5)
+
+            return v_tourner, omega_tourner
+
+        else:
+            #on actualise le vecteur rotation au cas ou on commencerait la
+            #rotation le prochain tour
+
+            self.strat_tourner=Turn90Strat(self.superviseur)
+
+        return v_avancer,omega_avancer
---
+++ superviseurs.py
@@ -0,0 +1,33 @@
+class Superviseur:
+
+    def __init__(self, robot, strategie):
+        """le superviseur sert d'intermediaire entre les controlleurs et le robot
+        le superviseur prendra une strategie en argument et executera une etape
+        a chaque appel de step.
+        Il traduira aussi les commandes pour faire bouger le robot
+        arguments : un robot, une strategie
+        """
+        self.robot = robot
+        self.strategie = strategie
+
+    def step(self,dt):
+
+        v, omega = self.strategie.get_command()
+        self.translate_command(v, omega)
+        self.robot.step(dt)
+
+    def translate_command(self,v,omega):
+
+        v_droite, v_gauche = self.calcul_dps(v, omega)
+        self.robot.set_dps(v_droite, v_gauche)
+
+    def calcul_dps(self,v ,omega):
+
+        v_droite = ((2*v)+(omega*self.robot.largeur))/ 2*self.robot.rayonroue
+        v_gauche = ((2*v)-(omega*self.robot.largeur))/ 2*self.robot.rayonroue
+
+        return v_droite, v_gauche
+
+    def redefine_strat(self,strategie):
+
+        self.strategie= strategie
